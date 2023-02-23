25f39165ea4954ae76742b1460a6aefe6d6e670a
Ben Clarkson <ben.a.clarkson@gmail.com>
2023-02-23 09:48:23 -0500

    Comment out duplicate solution to relieve error

--- Loops/do_while.js
+++ Loops/do_while.js
@@ -7,12 +7,12 @@
  * loop executes at least once.
 */
 
-let counter = 0;
+// let counter = 0;
 
-while (counter > 0) {
-  console.log('Woooot!');
-  counter -= 1;
-}
+// while (counter > 0) {
+//   console.log('Woooot!');
+//   counter -= 1;
+// }
 
 // ----------------------
 
